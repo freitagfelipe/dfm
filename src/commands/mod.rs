8d4342cb5c4fd3f46a194ab369e6e4ff37d398bc
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-03-04 20:33:32 -0300

    Add a Command trait for the CLI

---
+++ src/commands/mod.rs
@@ -0,0 +1,18 @@
+use colored::Colorize;
+
+pub trait Command: Sized {
+    type Error: std::error::Error;
+
+    fn execute(self) -> Result<&'static str, Self::Error>;
+
+    fn error(err: Self::Error) {
+        println!("{}", err.to_string().red());
+    }
+
+    fn call(self) {
+        match self.execute() {
+            Ok(message) => println!("{}", message.green()),
+            Err(err) => Self::error(err),
+        }
+    }
+}
