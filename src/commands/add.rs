b9e4d8ba097bb635f6350666e7d9a27c01372f29
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-03-04 21:49:45 -0300

    Update the error handling in the add command

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,9 +1,8 @@
 use super::Command;
 use crate::{setup::get_storage_folder_path, utils::check_if_file_exists};
 use clap::Args;
-use colored::Colorize;
 use std::path::Path;
-use std::process::{self, Command as Cmd, Stdio};
+use std::process::{Command as Cmd, Stdio};
 use std::{env, fs};
 use thiserror::Error;
 
@@ -13,6 +12,8 @@ pub enum Error {
     FileAlreadyAdded,
     #[error("File does not exists in the current folder")]
     FileDoesNotExists,
+    #[error("Something wrong happened: {0}")]
+    Unknown(String),
 }
 
 /// Add a file to the repository
@@ -21,55 +22,40 @@ pub struct Add {
     name: String,
 }
 
-fn execute_git_commands(storage_folder: &Path, file_name: &str) {
-    Cmd::new("git")
+fn execute_git_commands(storage_folder: &Path, file_name: &str) -> Result<(), Error> {
+    if let Err(err) = Cmd::new("git")
         .args(["add", "."])
         .current_dir(storage_folder)
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn()
-        .unwrap_or_else(|err| {
-            let err_message =
-                format!("The following error ocurred when trying to git add: {err}");
+    {
+        return Err(Error::Unknown(err.to_string()));
+    }
 
-            eprintln!("{}", err_message.red());
-
-            process::exit(1);
-        });
-
-    Cmd::new("git")
+    if let Err(err) = Cmd::new("git")
         .args(["commit", "-m", &format!("\"Add {file_name}\"")])
         .current_dir(storage_folder)
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn()
-        .unwrap_or_else(|err| {
-            let err_message =
-                format!("The following error ocurred when trying to git commit: {err}");
+    {
+        return Err(Error::Unknown(err.to_string()));
+    };
 
-            eprintln!("{}", err_message.red());
-
-            process::exit(1);
-        });
+    Ok(())
 }
 
 impl Command for Add {
     type Error = Error;
 
-
     fn execute(self) -> Result<&'static str, Self::Error> {
-        let storage_folder = get_storage_folder_path();
+        let storage_folder_path = get_storage_folder_path();
 
         let current_dir = match env::current_dir() {
             Ok(path) => path,
             Err(err) => {
-                let err_message = format!(
-                    "The following error ocurred when trying to get the current directory {err}"
-                );
-
-                eprintln!("{}", err_message.red());
-
-                process::exit(1);
+                return Err(Error::Unknown(err.to_string()));
             }
         };
 
@@ -77,32 +63,27 @@ impl Command for Add {
             return Err(Error::FileDoesNotExists);
         }
 
-        if check_if_file_exists(&storage_folder, &self.name) {
+        if check_if_file_exists(&storage_folder_path, &self.name) {
             return Err(Error::FileAlreadyAdded);
         }
 
         if fs::copy(
             current_dir.join(&self.name),
-            storage_folder.join(&self.name),
+            storage_folder_path.join(&self.name),
         )
         .is_err()
         {
-            eprintln!("{}", "An error ocurred while trying to copy the file".red());
-
-            process::exit(1);
+            return Err(Error::Unknown(
+                "An error ocurred while trying to copy the file".to_string(),
+            ));
         }
 
-        let storage_folder = fs::canonicalize(&storage_folder).unwrap_or_else(|err| {
-            let err_message = format!(
-                "The following error ocurred when trying to canonicalize the storage path: {err}"
-            );
+        let storage_folder_path = match fs::canonicalize(&storage_folder_path) {
+            Ok(path) => path,
+            Err(err) => return Err(Error::Unknown(err.to_string())),
+        };
 
-            eprintln!("{}", err_message.red());
-
-            process::exit(1);
-        });
-
-        execute_git_commands(&storage_folder, &self.name);
+        execute_git_commands(&storage_folder_path, &self.name)?;
 
         Ok("Successfully added the file")
     }
