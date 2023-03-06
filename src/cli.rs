af174b2a3e51ece1caca0ab60be1bc0de5646fc5
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-03-06 06:40:47 -0300

    Implement the CLI for the first commands

---
+++ src/cli.rs
@@ -0,0 +1,33 @@
+use crate::commands::{self, Command};
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Add(commands::Add),
+    List(commands::List),
+    Update(commands::Update),
+    Remove(commands::Remove),
+}
+
+/// DFM - Dot File Manager
+#[derive(Parser)]
+#[command(version)]
+pub struct CLI {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+impl Commands {
+    pub fn invoke(self) {
+        match self {
+            Self::Add(cmd) => cmd.call(),
+            Self::List(cmd) => cmd.call(),
+            Self::Update(cmd) => cmd.call(),
+            Self::Remove(cmd) => cmd.call(),
+        }
+    }
+}
+
+pub fn parse() -> CLI {
+    CLI::parse()
+}
