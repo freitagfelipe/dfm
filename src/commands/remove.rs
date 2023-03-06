e590a2c7048a45b47b9d89607d40824f7746741b
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-03-06 14:39:08 -0300

    Improve the error handling in the remove module

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -10,8 +10,8 @@ use thiserror::Error;
 pub enum Error {
     #[error("File does not exists in the repository")]
     FileDoesNotExists,
-    #[error("Something wrong happened: {0}")]
-    Unknown(String),
+    #[error("Something wrong happened: {0}, when trying to: {1}")]
+    Unknown(String, &'static str),
 }
 
 /// Removes a file of the repository
@@ -22,24 +22,29 @@ pub struct Remove {
 }
 
 fn execute_git_commands(storage_folder: &Path, file_name: &str) -> Result<(), Error> {
-    if let Err(err) = Cmd::new("git")
+    let mut handler = match Cmd::new("git")
         .args(["add", "."])
         .current_dir(storage_folder)
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn()
     {
-        return Err(Error::Unknown(err.to_string()));
+        Ok(handler) => handler,
+        Err(err) => return Err(Error::Unknown(err.to_string(), "execute git add .")),
+    };
+
+    if let Err(err) = handler.wait() {
+        return Err(Error::Unknown(err.to_string(), "wait git add finish"));
     }
 
     if let Err(err) = Cmd::new("git")
-        .args(["commit", "-m", &format!("\"Remove {file_name}\"")])
+        .args(["commit", "-m", &format!("Remove {file_name}")])
         .current_dir(storage_folder)
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .spawn()
     {
-        return Err(Error::Unknown(err.to_string()));
+        return Err(Error::Unknown(err.to_string(), "execute git commit"));
     };
 
     Ok(())
@@ -48,10 +53,15 @@ fn execute_git_commands(storage_folder: &Path, file_name: &str) -> Result<(), Er
 impl Command for Remove {
     type Error = Error;
 
-    fn execute(self) -> Result<&'static str, Self::Error> {
+    fn execute(self) -> Result<String, Self::Error> {
         let storage_folder_path = match get_storage_folder_path().canonicalize() {
             Ok(path) => path,
-            Err(err) => return Err(Error::Unknown(err.to_string())),
+            Err(err) => {
+                return Err(Error::Unknown(
+                    err.to_string(),
+                    "canonicalize the storage path",
+                ))
+            }
         };
 
         if !check_if_file_exists(&storage_folder_path, &self.name) {
@@ -59,11 +69,11 @@ impl Command for Remove {
         }
 
         if let Err(err) = fs::remove_file(storage_folder_path.join(&self.name)) {
-            return Err(Error::Unknown(err.to_string()));
+            return Err(Error::Unknown(err.to_string(), "remove a file"));
         }
 
         execute_git_commands(&storage_folder_path, &self.name)?;
 
-        Ok("Successfully removed the file")
+        Ok("Successfully removed the file".to_string())
     }
 }
